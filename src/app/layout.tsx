eac1b3bf266246472fc9690ba11ceae16b4c0bf4
Emil Krebs <emil.krebs@outlook.de>
2024-08-03 14:26:45 +0200

    added an opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,21 @@ import Navbar from "./components/navbar";
 export const metadata: Metadata = {
 	title: "BIPOC Climate Justice Summit",
 	description: "Join us at the BIPOC Climate Justice Summit on Sept 9-13. Registrations are open!",
+	creator: "Emil Krebs",
+	robots: "follow, index",
+	openGraph: {
+		type: "website",
+		title: "BIPOC Climate Justice Summit",
+		description: "Join us at the BIPOC Climate Justice Summit on Sept 9-13. Registrations are open!",
+		images: [
+			{
+				url: "/opengraph.webp",
+				width: 1200,
+				height: 630,
+				alt: "BIPOC Climate Justice Summit",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
