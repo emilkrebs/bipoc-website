249b8c451ef896a84830c3a2429b43a6f9d772e6
Emil Krebs <emil.krebs@outlook.de>
2024-09-02 14:19:23 +0200

    reactivated registrations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,9 @@ We view climate justice as a broad and inclusive concept that encompasses gender
 If you’re part of a collective, an activist, or looking to get involved, we’d love for you to join us! Check out our program and let us know if you’d like to participate.
 `;
 
+
+const registerLink = "https://form.jotform.com/241974035690057";
+
 export default function Home() {
 	return (
 		<main className="min-h-screen">
@@ -62,7 +65,7 @@ function LandingSection() {
 					</div>
 
 					<div className="flex flex-col items-center justify-center w-full gap-4 p-4">
-						<div className="px-4 py-2 text-2xl font-bold text-center text-red-500 bg-black border-2 border-red-500 rounded-lg shadow-lg bg-opacity-90">- Registration closed -</div>
+						<Link href={registerLink} className="px-4 py-2 text-2xl font-bold text-center text-red-500 bg-black border-2 border-red-500 rounded-lg shadow-lg bg-opacity-90">Register Now!</Link>
 
 						<div className="flex flex-row items-center justify-center gap-2 px-1 mt-2 bg-black bg-opacity-75 rounded-md">
 							<Link href="https://docs.google.com/spreadsheets/d/13K626izh8SPU73pMHAUd_dilP_uUZVPrTHFdv_sxCBA/edit?gid=521134214#gid=521134214" className="font-bold text-white hover:underline">View Schedule</Link>
