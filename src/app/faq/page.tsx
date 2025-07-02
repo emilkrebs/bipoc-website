b9e3ea4f691e264d9f2d674d2d36a160f1441f5d
Emil Krebs <emil.krebs@outlook.de>
2025-07-02 14:45:36 +0200

    minor improvements

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,7 +6,7 @@ export default function FaqPage() {
 		{
 			question: "Who can attend the summit?",
 			answer: `The summit is open to comrades who identify as Black, Indigenous, and/or People of Color and who align with the political grounding shared above. We welcome all BIPoC who are passionate about social justice issues, all of which relate back to climate and environment.
-Our conference is not open to white people.
+Our conference is not open to *white people*.
 `,
 			open: true,
 		},
